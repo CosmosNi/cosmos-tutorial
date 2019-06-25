b2fe70720ecd0c0da092a528e3e37383b9f320c0
倪佳晖 <nijiahui@kedacom.com>
2019-06-25 14:55:02 +0800

    1.consul的简单示例

---
+++ cosmos-boot/cosmos-consul/src/main/java/com/cosmos/consul/controller/TestController.java
@@ -0,0 +1,40 @@
+package com.cosmos.consul.controller;
+
+import com.cosmos.consuldemo.config.StudentConfig;
+import com.cosmos.consuldemo.feign.FeignService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ProjectName: consul-demo
+ * @Package: com.cosmos.consuldemo.controller
+ * @ClassName: TestController
+ * @Author: keda
+ * @Description: ${description}
+ * @Date: 2019/5/1 16:41
+ * @Version: 1.0
+ */
+@RestController
+public class TestController {
+    @Autowired
+    private FeignService feignService;
+    @Autowired
+    private StudentConfig studentConfig;
+
+    @GetMapping("/test")
+    public String test() {
+        System.out.println("------输出----------");
+        return "1232132132";
+    }
+
+    @GetMapping("/consul")
+    private String testConsul() {
+        return feignService.test();
+    }
+
+    @GetMapping("/config")
+    private String testConfig() {
+        return studentConfig.toString();
+    }
+}
